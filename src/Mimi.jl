c09c9dfc0532d3023f675ddeeeff97d4508b371c
David Anthoff <anthoff@berkeley.edu>
2016-03-25 19:59:29 -0700

    Remove old code from macro

--- src/Mimi.jl
+++ src/Mimi.jl
@@ -382,13 +382,6 @@ end
 Define a new component.
 """
 macro defcomp(name, ex)
-    dimdef = Expr(:block)
-    dimconstructor = Expr(:block)
-
-    pardef = Expr(:block)
-
-    vardef = Expr(:block)
-    varalloc = Expr(:block)
     resetvarsdef = Expr(:block)
 
     metavardef = Expr(:block)
@@ -398,9 +391,6 @@ macro defcomp(name, ex)
     for line in ex.args
         if line.head==:(=) && line.args[2].head==:call && line.args[2].args[1]==:Index
             dimensionName = line.args[1]
-
-            push!(dimdef.args,:($(esc(dimensionName))::$(esc(UnitRange{Int64}))))
-            push!(dimconstructor.args,:(s.$(dimensionName) = UnitRange{Int64}(1,indices[$(QuoteNode(dimensionName))])))
 
             push!(metadimdef.args, :(metainfo.adddimension(module_name(current_module()), $(Expr(:quote,name)), $(QuoteNode(dimensionName)) )))
         elseif line.head==:(=) && line.args[2].head==:call && line.args[2].args[1]==:Parameter
@@ -414,33 +404,18 @@ macro defcomp(name, ex)
                 error()
             end
 
-            concreteParameterType = parameterType == :Number ? :T : parameterType
-
             if any(l->isa(l,Expr) && l.head==:kw && l.args[1]==:index,line.args[2].args)
                 parameterIndex = first(filter(l->isa(l,Expr) && l.head==:kw && l.args[1]==:index,line.args[2].args)).args[2].args
-                partypedef = :(Array{$(concreteParameterType),$(length(parameterIndex))})
 
                 pardims = Array(Any, 0)
-                u = :(temp_indices = [])
                 for l in parameterIndex
-                    if isa(l, Symbol)
-                        push!(u.args[2].args, :(indices[$(QuoteNode(l))]))
-                    elseif isa(l, Int)
-                        push!(u.args[2].args, l)
-                    else
-                        error()
-                    end
                     push!(pardims, l)
                 end
 
                 push!(metapardef.args, :(metainfo.addparameter(module_name(current_module()), $(Expr(:quote,name)), $(QuoteNode(parameterName)), $(esc(parameterType)), $(pardims), "", "")))
             else
-                partypedef = concreteParameterType
-
                 push!(metapardef.args, :(metainfo.addparameter(module_name(current_module()), $(Expr(:quote,name)), $(QuoteNode(parameterName)), $(esc(parameterType)), [], "", "")))
             end
-
-            push!(pardef.args,:($(esc(parameterName))::$(esc(partypedef))))
         elseif line.head==:(=) && line.args[2].head==:call && line.args[2].args[1]==:Variable
             if isa(line.args[1], Symbol)
                 variableName = line.args[1]
@@ -452,43 +427,25 @@ macro defcomp(name, ex)
                 error()
             end
 
-            concreteVariableType = variableType == :Number ? :T : variableType
-
             if any(l->isa(l,Expr) && l.head==:kw && l.args[1]==:index,line.args[2].args)
                 variableIndex = first(filter(l->isa(l,Expr) && l.head==:kw && l.args[1]==:index,line.args[2].args)).args[2].args
-                vartypedef = :(Array{$(concreteVariableType),$(length(variableIndex))})
 
                 vardims = Array(Any, 0)
-                u = :(temp_indices = [])
                 for l in variableIndex
-                    if isa(l, Symbol)
-                        push!(u.args[2].args, :(indices[$(QuoteNode(l))]))
-                    elseif isa(l, Int)
-                        push!(u.args[2].args, l)
-                    else
-                        error()
-                    end
                     push!(vardims, l)
                 end
                 push!(metavardef.args, :(metainfo.addvariable(module_name(current_module()), $(Expr(:quote,name)), $(QuoteNode(variableName)), $(esc(variableType)), $(vardims), "", "")))
-
-                push!(varalloc.args,u)
-                push!(varalloc.args,:(s.$(variableName) = Array($(concreteVariableType),temp_indices...)))
 
                 if variableType==:Number
                     push!(resetvarsdef.args,:($(esc(symbol("fill!")))(s.Variables.$(variableName),$(esc(symbol("NaN"))))))
                 end
             else
-                vartypedef = concreteVariableType
                 push!(metavardef.args, :(metainfo.addvariable(module_name(current_module()), $(Expr(:quote,name)), $(QuoteNode(variableName)), $(esc(variableType)), [], "", "")))
 
                 if variableType==:Number
                     push!(resetvarsdef.args,:(s.Variables.$(variableName) = $(esc(symbol("NaN")))))
                 end
             end
-
-            push!(vardef.args,:($(esc(variableName))::$(esc(vartypedef))))
-
         elseif line.head==:line
         else
             error("Unknown expression.")
