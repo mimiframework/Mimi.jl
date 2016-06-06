daa18a9745c232f21945b84b8c9ecd3543539bf2
Cora Kingdon <ckingdon@berkeley.edu>
2016-06-05 18:53:45 -0700

    adds exception handling for tests that fail

--- test/test_dependencies.jl
+++ test/test_dependencies.jl
@@ -1,18 +1,40 @@
-using Base.Test
 using Mimi
 
+#list of URLs of branches of packages to test
 dependencies = []
 
-run(`mkdir tmp`)
+#list of failed tests to build as you go
+errors = []
+
+#make a temporary directory to run the tests in
+run(`mkdir tmp_testing`)
 cd("tmp")
 
+#loop through each dependent package
 for url in dependencies:
+  #download the package
   run(`wget $url`)
   zip_name = chomp(readall(`basename $url`))
   run(`unzip -x $zip_name`)
   run(`rm $zip_name`)
   package_name = chomp(readall(`ls`))
-  Pkg.test(package_name)
+  #test the package
+  try:
+    Pkg.test(package_name)
+  catch e:
+    append!(errors, [(package_name, e)])
+  #delete current package before testing next one
   run(`rm -rf *`)
 
-run(`rm -rf tmp`)
+#remove the temporary directory
+cd("../")
+run(`rm -rf tmp_testing`)
+
+#report the errors that occurred
+num_errors = length(errors)
+if num_errors == 1:
+  println("1 test failed:")
+else:
+  println("$num_errors tests failed:")
+for package_name, error in errors:
+  println("error in $package_name:" + e)
