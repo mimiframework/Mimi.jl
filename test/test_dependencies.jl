f8f475bb6a6d9a917c18118b80248c42d33534c4
Cora Kingdon <ckingdon@berkeley.edu>
2016-06-13 00:53:17 -0700

    incorporates previous comments from davidanthoff

--- test/test_dependencies.jl
+++ test/test_dependencies.jl
@@ -1,6 +1,5 @@
 using Mimi
-
-Pkg.add("InfoZIP")
+using InfoZIP
 
 #list of URLs of branches of packages to test
 dependencies = [
@@ -10,21 +9,21 @@ dependencies = [
 #list of failed tests to build as you go
 errors = []
 #make a temporary directory to run the tests in
-tmp_path = joinpath(dirname(@__FILE__),"tmp_testing/")
+tmp_path = joinpath(dirname(@__FILE__),"tmp_testing")
 mkdir(tmp_path)
-cd(tmp_path)
 
 #loop through each dependent package
 for url in dependencies
   #download the package
-  download(url)
+  download(url, tmp_path)
   zip_name = chomp(basename(url))
   unzip(zip_name, tmp_path)
   rm(string(tmp_path, zip_name))
-  package_name = chomp(readall(`ls`))
+  package_name = chomp(readir())
   #test the package
   try
-    include(string(tmp_path, package_name, "/test/runtests.jl"))
+    process = string(tmp_path, package_name, "/test/runtests.jl")
+    run(`julia $process`)
   catch e
     append!(errors, [(package_name, e)])
   end
@@ -37,18 +36,12 @@ rm(tmp_path, recursive=true)
 
 #report the errors that occurred
 num_errors = length(errors)
-if num_errors == 1
-  error_message = "1 test failed \n:
-  "
-else
-  error_message = "$num_errors tests failed: \n"
-end
+error_message = "Failed tests: $num_errors"
 
-for item in errors
-  package_name, error = item
+for (package_name, error) in errors
   error_message = string(error_message, "error in $package_name:", error, "\n")
 end
 
 if num_errors > 0
-  throw(Exception: error_message)
+  error(error_message)
 end
