ee575c94f05f97f94b116653f0e11f83761fb584
David Anthoff <anthoff@berkeley.edu>
2016-06-22 15:37:28 -0700

    Fix dependencies for doc publishing

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,7 @@ using Documenter, Mimi
 makedocs()
 
 deploydocs(
+    deps = Deps.pip("pygments", "mkdocs", "mkdocs-material", "python-markdown-math"),
     repo = "github.com/anthofflab/Mimi.jl.git",
     julia = "0.4"
 )
