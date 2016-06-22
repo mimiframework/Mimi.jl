fb9ff73b04a34463bd242123f4bf6f4752de05b9
Cora Kingdon <ckingdon@berkeley.edu>
2016-06-22 11:36:16 -0700

    Add dependency testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Mimi
 using Base.Test
 
-tests = ["main", "references", "units"]
+tests = ["main", "references", "units", "dependencies"]
 
 for t in tests
     fp = joinpath("test_$t.jl")
---
+++ test/test_dependencies.jl
@@ -0,0 +1,52 @@
+using Mimi
+using InfoZIP
+
+#list of URLs of branches of packages to test
+dependencies = [
+"https://github.com/davidanthoff/fund.jl/archive/master.zip"
+]
+
+#list of failed tests to build as you go
+errors = []
+#make a temporary directory to run the tests in
+tmp_path = joinpath(dirname(@__FILE__),"tmp_testing/")
+mkdir(tmp_path)
+
+#loop through each dependent package
+for url in dependencies
+  #download the package
+  zip_name = chomp(basename(url))
+  zip_file_path = joinpath(tmp_path, zip_name)
+  download(url, zip_file_path)
+  InfoZIP.unzip(zip_file_path, tmp_path)
+  rm(zip_file_path)
+  #find the name of the unzipped package (this only works if the zip archive only has one directory, the package)
+  package_name = readdir(tmp_path)[1]
+
+  #test the package
+  try
+    process = string(tmp_path, package_name, "/test/runtests.jl")
+    run(`julia $process`)
+  catch e
+    append!(errors, [(package_name, e)])
+  end
+  #delete current package before testing next one
+  rm(joinpath(tmp_path, package_name), recursive=true)
+end
+
+#remove the temporary directory
+rm(tmp_path, recursive=true)
+
+#report the errors that occurred
+num_errors = length(errors)
+error_message = "Failed tests: $num_errors"
+
+for (package_name, error) in errors
+  error_message = string(error_message, "\n", "error in $package_name:", error)
+end
+
+if num_errors > 0
+  error(error_message)
+else
+  println("All dependency tests passed.")
+end
