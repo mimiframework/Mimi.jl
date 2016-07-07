14b39a49f1ee73464f15488e9a7a4851eda948be
David Anthoff <anthoff@berkeley.edu>
2016-07-07 11:32:26 -0700

    Add lint_helper function

--- src/Mimi.jl
+++ src/Mimi.jl
@@ -14,6 +14,16 @@ export
 import
     Base.getindex, Base.run
 
+function lint_helper(ex::Expr, ctx)
+    if ex.head == :macrocall
+        if ex.args[1] == Symbol("@defcomp")
+            push!(ctx.callstack[end].types, ex.args[2])
+            return true
+        end
+    end
+    return false
+end
+
 abstract ComponentState
 
 abstract Parameter
