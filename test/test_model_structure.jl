da6b44f78041da51d9a355d0341db71d5d57ac51
Cora Kingdon <ckingdon@berkeley.edu>
2016-08-02 21:56:29 -0700

    adds more unit tests to cover component/connections functionality

--- test/test_model_structure.jl
+++ test/test_model_structure.jl
@@ -1,23 +1,29 @@
-using Base.test
+#tests the framework of components and connections without actually running the model
+
+using Base.Test
 using Mimi
 
 @defcomp A begin
-  varA = Variable()
-  parA = Parameter()
+  varA = Variable(index=[time])
+  parA = Parameter(index=[time])
 end
 
 @defcomp B begin
-  varB = Variable()
-  parB = Parameter()
+  varB = Variable(index=[time])
+  parB = Parameter(index=[time])
 end
 
 m = Model()
+setindex(m, :time, [2015:5:2110])
+
 addcomponent(m, A)
 addcomponent(m, B)
 
 connectparameter(m, :A, :parA, :B, :varB)
 
-@test length(m.nodes)==2
-@test length(m.edges)==1
-@test get_connections(m, :A, :INCOMING)[0].source_component_name == :B
-@test length(get_connections(m, :B, :INCOMING)) == 0
+@test length(m.components)==2
+@test length(m.connections)==1
+@test get_connections(m, :A, :incoming)[1].source_component_name == :B
+@test length(get_connections(m, :B, :incoming)) == 0
+@test get_connections(m, :B, :outgoing)[1].target_component_name == :A
+@test length(get_connections(m, :A, :all)) == 1
