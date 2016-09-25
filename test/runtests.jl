b766f81ec82ee876b680458e237d6b85041b590c
Cora Kingdon <ckingdon@berkeley.edu>
2016-09-25 15:49:00 -0700

    test without references

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,8 @@
 using Mimi
 using Base.Test
 
-tests = ["main", "references", "units", "dependencies", "model_structure"]
+#tests = ["main", "references", "units", "dependencies", "model_structure"]
+tests = ["main", "units", "dependencies", "model_structure"]
 
 for t in tests
     fp = joinpath("test_$t.jl")
