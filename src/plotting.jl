1ac8f7a9f3884b88864b83171ca213c675e3f9c9
Scott Werwath <swerwath@berkeley.edu>
2016-10-17 11:57:28 -0700

    Skeleton for plotting

--- src/plotting.jl
+++ src/plotting.jl
@@ -1,1 +1,12 @@
 using Plots
+
+function line_plot(m::Model component::Symbol, parameter::Symbol, index::Symbol = :time, legend::Symbol = nothing, xlabel = string(index), ylabel = string(parameter))
+  if isnull(m.mi)
+      run(m)
+  end
+
+  pyplot()
+  plot(m.indices_values[index], m[component, parameter])
+  
+
+end
