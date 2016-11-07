da66b93b271c438055152e5b782561c79a2460a3
Scott Werwath <swerwath@berkeley.edu>
2016-11-07 11:06:34 -0800

    Changed indentation

--- src/plotting.jl
+++ src/plotting.jl
@@ -6,43 +6,43 @@ Extends the Plots module to be able to take a model information parameters for
 convenience. More advanced plotting may require accessing the Plots module directly.
 """
 function Plots.plot(m::Model, component::Symbol, parameter::Symbol ; index::Symbol = :time, legend::Symbol = nothing, x_label = string(index), y_label = string(parameter))
-  if isnull(m.mi)
-    error("A model must be run before it can be plotted")
-  end
-
-  # Create axis labels
-  try
-    units = getmetainfo(m, component).parameters[parameter].unit
-    units = "[$(units)]"
-  catch
-    units = ""
-  end
-
-  # Convert labels from camel case/snake case
-  if x_label == string(index)
-    x_label = prettifystring(x_label)
-  end
-
-  if y_label == string(parameter)
-    y_label = prettifystring(y_label)
-  end
-
-  x_label = "$(x_label) $(units)"
-  y_label = "$(y_label) $(units)"
-
-  plt = plot() # Clear out any previous plots
-
-  if is(legend, nothing)
-    # Assume that we are only plotting one line (i.e. it's not split up by regions)
-    plot(plt, m.indices_values[index], m[component, parameter], xlabel=x_label, ylabel=y_label)
-  else
-    # For multiple lines, we need to read the legend labels from legend
-    for line_index in 1:size(m[component, parameter])[2] # TODO: Check that these dimensions match
-      plot!(plt, m.indices_values[index], m[component, parameter][:,line_index], label = m.indices_values[legend][line_index], xlabel=x_label, ylabel=y_label)
+    if isnull(m.mi)
+        error("A model must be run before it can be plotted")
     end
-  end
 
-  return plt
+    # Create axis labels
+    try
+        units = getmetainfo(m, component).parameters[parameter].unit
+        units = "[$(units)]"
+    catch
+        units = ""
+    end
+
+    # Convert labels from camel case/snake case
+    if x_label == string(index)
+        x_label = prettifystring(x_label)
+    end
+
+    if y_label == string(parameter)
+        y_label = prettifystring(y_label)
+    end
+
+    x_label = "$(x_label) $(units)"
+    y_label = "$(y_label) $(units)"
+
+    plt = plot() # Clear out any previous plots
+
+    if is(legend, nothing)
+        # Assume that we are only plotting one line (i.e. it's not split up by regions)
+        plot(plt, m.indices_values[index], m[component, parameter], xlabel=x_label, ylabel=y_label)
+    else
+        # For multiple lines, we need to read the legend labels from legend
+        for line_index in 1:size(m[component, parameter])[2] # TODO: Check that these dimensions match
+            plot!(plt, m.indices_values[index], m[component, parameter][:,line_index], label = m.indices_values[legend][line_index], xlabel=x_label, ylabel=y_label)
+        end
+    end
+
+    return plt
 end
 
 """
@@ -53,25 +53,25 @@ understanding of Julia's implementation of regex), cannot handle camelcase strin
 with more than 2 consecutive capitals, e.g. fileInTXTFormat -> File In T X T Format
 """
 function prettifystring(s::String)
-  if contains(s, "_")
-    # Snake Case
-    s = replace(s, r"_", s" ")
-  else
-    # Camel Case
-    s = replace(s, r"([a-z])([A-Z])", s"\1 \2")
-    s = replace(s, r"([A-Z])([A-Z])", s"\1 \2")
-  end
+    if contains(s, "_")
+        # Snake Case
+        s = replace(s, r"_", s" ")
+    else
+        # Camel Case
+        s = replace(s, r"([a-z])([A-Z])", s"\1 \2")
+        s = replace(s, r"([A-Z])([A-Z])", s"\1 \2")
+    end
 
-  # Capitalize the first letter of each word
-  s_arr = split(s)
-  to_ret = ""
-  for word in s_arr
-    word_caps = "$(uppercase(word[1]))$(word[2:length(word)])"
-    to_ret = "$(to_ret)$(word_caps) "
-  end
+    # Capitalize the first letter of each word
+    s_arr = split(s)
+    to_ret = ""
+    for word in s_arr
+        word_caps = "$(uppercase(word[1]))$(word[2:length(word)])"
+        to_ret = "$(to_ret)$(word_caps) "
+    end
 
-  # Return our string, minus the trailing space that was added
-  return to_ret[1:length(to_ret) - 1]
+    # Return our string, minus the trailing space that was added
+    return to_ret[1:length(to_ret) - 1]
 end
 
 # End plotting section
