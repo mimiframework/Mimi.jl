8815deebc21fbcb04672eb7df5aefb9e3690f848
Cora Kingdon <ckingdon@berkeley.edu>
2016-11-19 15:18:48 -0800

    parameter_labels

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Mimi
 using Base.Test
 
-tests = ["main", "references", "units", "model_structure", "parameter_checking"]
+tests = ["main", "references", "units", "model_structure", "parameter_labels"]
 
 for t in tests
     fp = joinpath("test_$t.jl")
