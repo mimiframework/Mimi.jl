9c81a5a4296ee6061db297f095a992c8e7eeac7b
GIRISH BALAJI <girishb@berkeley.edu>
2017-02-09 23:56:00 -0800

    fixed test_arguments in runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Mimi
 using Base.Test
 
-tests = ["main", "references", "units", "model_structure", "tools", "parameter_labels", "adder", "test_arguments"]
+tests = ["main", "references", "units", "model_structure", "tools", "parameter_labels", "adder", "arguments"]
 
 for t in tests
     fp = joinpath("test_$t.jl")
