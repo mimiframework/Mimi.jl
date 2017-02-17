e2d4b726fed74a0eb6cabfe3b0a2aed052ecb448
David Anthoff <anthoff@berkeley.edu>
2017-02-17 15:58:49 -0800

    Rearrange test list to avoid future merge conflicts

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,19 @@ using Mimi
 using Base.Test
 
 
-tests = ["main", "references", "units", "model_structure", "tools", "parameter_labels", "marginal_models", "adder", "getindex", "num_components", "components_ordering", "variables_model_instance"]
+tests = ["main",
+    "references",
+    "units",
+    "model_structure",
+    "tools",
+    "parameter_labels",
+    "marginal_models",
+    "adder",
+    "getindex",
+    "num_components",
+    "components_ordering",
+    "variables_model_instance"
+]
 
 for t in tests
     fp = joinpath("test_$t.jl")
