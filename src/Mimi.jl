6536e6179ad8c00fd2943953888bb90d4ea2cf87
GIRISH BALAJI <girishb@berkeley.edu>
2017-02-17 00:30:47 -0800

    figured out how get variables given a modelinstance and a componentname

--- src/Mimi.jl
+++ src/Mimi.jl
@@ -376,11 +376,11 @@ function getindex(m::Model, component::Symbol, name::Symbol)
 end
 
 function getindex(mi::ModelInstance, component::Symbol, name::Symbol)
-    if (component not in mi.components)
+    if !(component in keys(mi.components))
         error("Component does not exist in current model")
     end
-    if (name not in mi.components.Variables)
-        error("Variable does not exists in this component")
+    if !(name in fieldnames(mi.components[component].Variables))
+        error("Variable does not exist in this component")
     end
     return getfield(mi.components[component].Variables, name)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using Mimi
 using Base.Test
 
-tests = ["main", "references", "units", "model_structure", "tools", "parameter_labels", "marginal_models", "adder"]
+tests = ["main", "references", "units", "model_structure", "tools", "parameter_labels", "marginal_models", "adder", "getindex"]
 
 for t in tests
     fp = joinpath("test_$t.jl")
