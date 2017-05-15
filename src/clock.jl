745a1d9323669cf445bb269f1495a74f07377128
Cora Kingdon <ckingdon@berkeley.edu>
2017-05-15 16:29:22 -0700

    creates OurTVector and OurTMatrix

--- src/clock.jl
+++ src/clock.jl
@@ -1,3 +1,5 @@
+using OffsetArrays
+
 immutable Timestep{Offset, Final}
 	t::Int
 
@@ -56,4 +58,38 @@ end
 
 function getnewtimestep{Offset, Final}(ts::Timestep{Offset, Final}, newoffset::Int)
 	return Timestep{newoffset, Final}(ts.t + Offset - 1 )
+end
+
+function getobjectivetime{Offset, Final}(ts::Timestep{Offset, Final})
+	return ts.t + Offset - 1
+end
+
+type OurTVector{T, Offset} #don't need to encode N (number of dimensions) as a type parameter because we are hardcoding it as 1 for the vector case
+	data::OffsetArray{T, 1, Array{T, 1}}
+
+	function OurTVector(data::Array{T,1})
+		d = OffsetArray(data, Offset:(Offset+length(data)-1))
+		x = new{T, Offset}(d)
+		return x
+	end
+end
+
+type OurTMatrix{T, Offset} #don't need to encode N (number of dimensions) as a type parameter because we are hardcoding it as 2 for the matrix case
+	data::OffsetArray{T, 2, Array{T, 2}}
+
+	function OurTMatrix(data::Array{T,2})
+		d = OffsetArray(data, Offset:(Offset+size(data)[1]-1), 1:size(data)[2])
+		x = new{T, Offset}(d)
+		return x
+	end
+end
+
+function Base.getindex{T, d_offset, t_offset, Final}(x::OurTVector{T, d_offset}, ts::Timestep{t_offset, Final})
+	t = getobjectivetime(ts)
+	return x.data[t]
+end
+
+function Base.getindex{T, d_offset, t_offset, Final}(x::OurTMatrix{T, d_offset}, ts::Timestep{t_offset, Final}, i::Int)
+	t = getobjectivetime(ts)
+	return x.data[t, i]
 end
