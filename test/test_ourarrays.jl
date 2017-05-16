244d14902c04530e597f674e41f421a3c40e3fe0
Cora Kingdon <ckingdon@berkeley.edu>
2017-05-15 22:26:07 -0700

    mistake in test file

--- test/test_ourarrays.jl
+++ test/test_ourarrays.jl
@@ -7,12 +7,12 @@ a = collect(reshape(1:16,4,4))
 #  Test OurTVector  #
 #####################
 
-x = OurTVector{Int, 2000}(a[:,3])
-t = Timestep{2001,3000}(2001)
+x = Mimi.OurTVector{Int, 2000}(a[:,3])
+t = Mimi.Timestep{2001,3000}(2001)
 
 @test x[t] == 10
 
-t2 = getnexttimestep(t)
+t2 = Mimi.getnexttimestep(t)
 
 @test x[t2] == 11
 
@@ -20,7 +20,7 @@ t2 = getnexttimestep(t)
 #  Test OurTMatrix  #
 #####################
 
-y = OurTMatrix{Int, 2000}(a[:,1:2])
+y = Mimi.OurTMatrix{Int, 2000}(a[:,1:2])
 
 @test y[t,1] == 2
 @test y[t,2] == 6
