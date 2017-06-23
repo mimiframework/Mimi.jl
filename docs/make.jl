92c5a56877ef1cb27c93b35d8f2bb55bfc8ac0c8
Cora Kingdon <ckingdon@berkeley.edu>
2017-06-23 12:32:59 -0700

    switch order

--- docs/make.jl
+++ docs/make.jl
@@ -7,8 +7,8 @@ makedocs(
 	pages = [
 		"Home" => "index.md",
 		"Installation Guide" => "installation.md",
+        "User Guide" => "userguide.md",
 		"Tutorial" => "tutorial.md",
-		"User Guide" => "userguide.md",
 		"FAQ" => "faq.md",
 		"Reference" => "reference.md"]
 )
