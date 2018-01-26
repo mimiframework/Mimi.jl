cd0eaf4e37c49241f7111b680c60e7a6ed01bf1d
lrennels <lrennels@berkeley.edu>
2018-01-26 15:31:51 -0800

    added scrollbar and sizing

--- src/explore/variabledata.js
+++ src/explore/variabledata.js
@@ -22,7 +22,9 @@ var varList =
             "encoding": {
                 "x": { "field": "Region", "type": "ordinal" },
                 "y": { "field": "Quantity", "type": "quantitative" }
-            }
+            },
+            "width": 400,
+            "height": 400,
         }
     },
 
@@ -43,7 +45,9 @@ var varList =
             "encoding": {
                 "x": { "field": "Time", "type": "temporal", "Axis" : {"format": "%Y}}" }},
                 "y": { "field": "Quantity", "type": "quantitative" }
-            }
+            },
+            "width": 400,
+            "height": 400,
         }
     },
 
@@ -69,9 +73,11 @@ var varList =
                 "x": {"field": "Time", "type": "temporal"},
                 "y": {"field": "Quantity", "type": "quantitative"},
                 "color": {"field": "symbol", "type": "nominal"}
-            }
+            },
+            "width": 400,
+            "height": 400,
         }
-    }, 
+    },
 
     {
         name:  "Var 4", 
@@ -107,7 +113,191 @@ var varList =
             "color": {"field":"label", "type":"nominal", "scale": {"range": ["#e6aa06", "#1f77b4"]}}
             },
             "config": {"mark": {"stacked": "normalize"}
-            }
+        },
+        "width": 400,
+        "height": 400,
+    }
+},
+
+    {
+        name: "Var 5",
+        VLSpect: {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+            "description": "A simple bar chart for regional data.",
+            "title": "Var 1",
+            "data": {
+                "values": [
+                    { "Region": "A", "Quantity": 28 }, { "Region": "B", "Quantity": 55 }, { "Region": "C", "Quantity": 43 },
+                    { "Region": "D", "Quantity": 91 }, { "Region": "E", "Quantity": 81 }, { "Region": "F", "Quantity": 53 },
+                    { "Region": "G", "Quantity": 19 }, { "Region": "H", "Quantity": 87 }, { "Region": "I", "Quantity": 52 }
+                ]
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": { "field": "Region", "type": "ordinal" },
+                "y": { "field": "Quantity", "type": "quantitative" }
+            },
+            "width": 400,
+            "height": 400,
+        }
+    },
+    {
+        name: "Var 6",
+        VLSpect: {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+            "description": "A simple bar chart for regional data.",
+            "title": "Var 1",
+            "data": {
+                "values": [
+                    { "Region": "A", "Quantity": 28 }, { "Region": "B", "Quantity": 55 }, { "Region": "C", "Quantity": 43 },
+                    { "Region": "D", "Quantity": 91 }, { "Region": "E", "Quantity": 81 }, { "Region": "F", "Quantity": 53 },
+                    { "Region": "G", "Quantity": 19 }, { "Region": "H", "Quantity": 87 }, { "Region": "I", "Quantity": 52 }
+                ]
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": { "field": "Region", "type": "ordinal" },
+                "y": { "field": "Quantity", "type": "quantitative" }
+            },
+            "width": 400,
+            "height": 400,
+        }
+    },
+    {
+        name: "Var 7",
+        VLSpect: {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+            "description": "A simple bar chart for regional data.",
+            "title": "Var 1",
+            "data": {
+                "values": [
+                    { "Region": "A", "Quantity": 28 }, { "Region": "B", "Quantity": 55 }, { "Region": "C", "Quantity": 43 },
+                    { "Region": "D", "Quantity": 91 }, { "Region": "E", "Quantity": 81 }, { "Region": "F", "Quantity": 53 },
+                    { "Region": "G", "Quantity": 19 }, { "Region": "H", "Quantity": 87 }, { "Region": "I", "Quantity": 52 }
+                ]
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": { "field": "Region", "type": "ordinal" },
+                "y": { "field": "Quantity", "type": "quantitative" }
+            },
+            "width": 400,
+            "height": 400,
+        }
+    },
+    {
+        name: "Var 8",
+        VLSpect: {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+            "description": "A simple bar chart for regional data.",
+            "title": "Var 1",
+            "data": {
+                "values": [
+                    { "Region": "A", "Quantity": 28 }, { "Region": "B", "Quantity": 55 }, { "Region": "C", "Quantity": 43 },
+                    { "Region": "D", "Quantity": 91 }, { "Region": "E", "Quantity": 81 }, { "Region": "F", "Quantity": 53 },
+                    { "Region": "G", "Quantity": 19 }, { "Region": "H", "Quantity": 87 }, { "Region": "I", "Quantity": 52 }
+                ]
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": { "field": "Region", "type": "ordinal" },
+                "y": { "field": "Quantity", "type": "quantitative" }
+            },
+            "width": 400,
+            "height": 400,
+        }
+    },
+    {
+        name: "Var 9",
+        VLSpect: {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+            "description": "A simple bar chart for regional data.",
+            "title": "Var 1",
+            "data": {
+                "values": [
+                    { "Region": "A", "Quantity": 28 }, { "Region": "B", "Quantity": 55 }, { "Region": "C", "Quantity": 43 },
+                    { "Region": "D", "Quantity": 91 }, { "Region": "E", "Quantity": 81 }, { "Region": "F", "Quantity": 53 },
+                    { "Region": "G", "Quantity": 19 }, { "Region": "H", "Quantity": 87 }, { "Region": "I", "Quantity": 52 }
+                ]
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": { "field": "Region", "type": "ordinal" },
+                "y": { "field": "Quantity", "type": "quantitative" }
+            },
+            "width": 400,
+            "height": 400,
+        }
+    },
+    {
+        name: "Var 10",
+        VLSpect: {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+            "description": "line graph for time series for several regions",
+            "title": "Var 3",
+            "data": {
+                "values": [
+                    { "Time": 1, "Quantity": 28, "symbol": "A"}, { "Time": 2, "Quantity": 55, "symbol": "A" }, { "Time": 3, "Quantity": 43, "symbol": "A" },
+                    { "Time": 4, "Quantity": 91, "symbol": "A" }, { "Time": 5, "Quantity": 81, "symbol": "A" }, { "Time": 6, "Quantity": 53, "symbol": "A" },
+                    { "Time": 7, "Quantity": 19, "symbol": "A"}, { "Time": 8, "Quantity": 87, "symbol": "A" }, { "Time": 9, "Quantity": 52, "symbol": "A" },
+                
+                    { "Time": 1, "Quantity": 120, "symbol": "B"}, { "Time": 2, "Quantity": 65, "symbol": "B" }, { "Time": 3, "Quantity": 67, "symbol": "A" },
+                    { "Time": 4, "Quantity": 52, "symbol": "B" }, { "Time": 5, "Quantity": 91, "symbol": "B" }, { "Time": 6, "Quantity": 73, "symbol": "B" },
+                    { "Time": 7, "Quantity": 89, "symbol": "B"}, { "Time": 8, "Quantity": 107, "symbol": "B" }, { "Time": 9, "Quantity": 83, "symbol": "B" }    
+                ]
+            },
+            "mark": "line",
+            "encoding": {
+                "x": {"field": "Time", "type": "temporal"},
+                "y": {"field": "Quantity", "type": "quantitative"},
+                "color": {"field": "symbol", "type": "nominal"}
+            },
+            "width": 400,
+            "height": 400,
+        }
+    },
+    {
+        name: "Var 11",
+        VLSpect: {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+            "description": "A simple bar chart for regional data.",
+            "title": "Var 1",
+            "data": {
+                "values": [
+                    { "Region": "A", "Quantity": 28 }, { "Region": "B", "Quantity": 55 }, { "Region": "C", "Quantity": 43 },
+                    { "Region": "D", "Quantity": 91 }, { "Region": "E", "Quantity": 81 }, { "Region": "F", "Quantity": 53 },
+                    { "Region": "G", "Quantity": 19 }, { "Region": "H", "Quantity": 87 }, { "Region": "I", "Quantity": 52 }
+                ]
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": { "field": "Region", "type": "ordinal" },
+                "y": { "field": "Quantity", "type": "quantitative" }
+            },
+            "width": 400,
+            "height": 400,
+        }
+    },
+    {
+        name: "Var 12",
+        VLSpect: {
+            "$schema": "https://vega.github.io/schema/vega-lite/v2.0.json",
+            "description": "A simple bar chart for regional data.",
+            "title": "Var 1",
+            "data": {
+                "values": [
+                    { "Region": "A", "Quantity": 28 }, { "Region": "B", "Quantity": 55 }, { "Region": "C", "Quantity": 43 },
+                    { "Region": "D", "Quantity": 91 }, { "Region": "E", "Quantity": 81 }, { "Region": "F", "Quantity": 53 },
+                    { "Region": "G", "Quantity": 19 }, { "Region": "H", "Quantity": 87 }, { "Region": "I", "Quantity": 52 }
+                ]
+            },
+            "mark": "bar",
+            "encoding": {
+                "x": { "field": "Region", "type": "ordinal" },
+                "y": { "field": "Quantity", "type": "quantitative" }
+            },
+            "width": 400,
+            "height": 400,
         }
     }
 ]
