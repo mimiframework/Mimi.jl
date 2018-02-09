7a3fb566ad109e3ee54860611119b9c0dc3d7317
lrennels <lrennels@berkeley.edu>
2018-02-08 16:54:50 -0800

    removed explore function for functionality purposes

--- src/explore/explore.jl
+++ src/explore/explore.jl
@@ -8,22 +8,23 @@ using Blink
 ##TODO (version 0.1) decide how to plot single value parameters or values
 ##TODO (version 0.2) add parameters function to mimi_core.jl
 
-function explore(model)
-    #get variable data
-    include("buildspecs.jl")
-    speclist = getspeclist(model)
-
-    #start Blink window
-    w = Blink.Window()
-
-    #load main html file
-    # joinpath(@__DIR__, "main.html")
-    loadfile(w, abspath("main.html"))
-    
-    #refresh variable list
-    @js w refresh($speclist)
-end
-
 #run tutorial (included for ease of developement since tutorials need to be 
 #updated)
 include("one-region-model.jl")
+
+#get variable data
+include("buildspecs.jl")
+speclist = getspeclist(my_model)
+
+#start Blink window
+w = Blink.Window()
+
+#load main html file
+# joinpath(@__DIR__, "main.html")
+loadfile(w, abspath("main.html"))
+    
+#refresh variable list
+@js w refresh($speclist)
+
+
+
