6a30349451b5d89fb6e60f295039160177ea4d01
lrennels <lrennels@berkeley.edu>
2018-02-26 12:06:59 -0800

    switching from Blink to Electron

--- src/explorer/explore.jl
+++ src/explorer/explore.jl
@@ -1,25 +1,30 @@
 ## Mimi UI
 using DataFrames
 using JSON
-using Blink
+using Electron
 
 #function to get variable data
 include("buildspecs.jl")
 include("getparameters.jl")
 
 function explore(model)
+
     #get variable data
     speclist = getspeclist(model)
+    speclistJSON = JSON.json(speclist)
 
-    #start Blink window
-    w = Blink.Window()
+    #start Electron app
+    if !isdefined(:app)
+        global app = Application()
+    end
 
     #load main html file
-    loadfile(w, joinpath(@__DIR__, "assets", "main.html"))
-        
-    #refresh variable list
-    @js w refresh($speclist)
+    mainpath = joinpath(@__DIR__, "assets", "main.html")
+    w = Window(app, URI('${mainpath}'))
 
+    #refresh variable list
+    result = run(app, refresh(speclistJSON))
+    
 end
 
 
