254188f84a7c88e0f78f2e88e94605528ffe7b93
lrennels <lrennels@berkeley.edu>
2018-03-26 11:56:53 -0700

    Update one component tutorial file

--- examples/01-onecomponent.jl
+++ examples/01-onecomponent.jl
@@ -3,29 +3,41 @@ using Mimi
 # Define a simple component
 # =========================
 
-# First define the state this component will hold
-
 @defcomp component1 begin
+
+    # First define the state this component will hold
     savingsrate = Parameter()
 
+    # Second, define the (optional) init function for the component
+    function init(p, v, d)
+    end
+
+    # Third, define the run_timestep function for the component
+    function run_timestep(p, v, d, t)
+    end
+
 end
 
-# Second, define the run_timestep function for the component
-
-function run_timestep(s::component1, t::Int)
-end
 
 # Create a model uses the component
 # =================================
 
-m = Model()
+@defmodel begin
 
-addcomponent(m, component1)
+    m = Model()
+
+    component(component1)
+end
 
 # Run model
 # =========
 
 run(m)
+
+# Explore the model variables and parameters with the explorer UI
+# =================================
+
+explore(m)
 
 # Access the variables in the model
 # =================================
