84be16d433e5ed3f4389cd1e57db2154b3a1f888
David Anthoff <anthoff@berkeley.edu>
2018-05-01 11:03:52 -0700

    Update diag example.jl

--- wip/example.jl
+++ wip/example.jl
@@ -23,19 +23,25 @@ run(m)
 
 @benchmark run(m)
 
-function Mimi.run_timestep(::Val{:Main}, ::Val{:foo}, p::Mimi.ComponentInstanceParameters, v::Mimi.ComponentInstanceVariables, d::Dict{Symbol, Vector{Int}}, t)
+function Mimi.run_timestep(::Val{:Main}, ::Val{:foo}, p::Mimi.ComponentInstanceParameters, v::Mimi.ComponentInstanceVariables, d::Dict{Symbol, Vector{Int}}, t::Union{Int64, Mimi.Timestep})
     println("WE ARE IN $t")
     println(typeof(p))
-    #a = Mimi.getproperty(p, Val(:in))
-    aaa = p.values
-    aa = aaa[1]
-    a = aa[]
+    a = Mimi.getproperty(p, Val(:in))
+    # aaa = p.values
+    # aa = aaa[1]
+    # a = aa[]
     b = a[t]
     (Mimi.getproperty(v, Val(:out)))[t] = b
 end
+
+# Base.arrayref(Core.getfield(Core.getfield(Base.getfield(getfield(p, :values), 1), :x), :data), t)
 
 p = m.mi.components[:foo].parameters
 
 v = m.mi.components[:foo].variables
 
 @code_warntype Mimi.run_timestep(Val(:Main), Val(:foo), p, v, Dict{Symbol,Vector{Int}}(), 2)
+
+cl = Mimi.make_clock(m.mi, 4, [1,2,3,4])
+
+@code_warntype run_timestep(Val(:Main),Val(:foo), m.mi.components[:foo], cl)
