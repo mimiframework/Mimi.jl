2082ac88038fd6ec4be852c30a8e03a78df51a06
Richard Plevin <rich@plevin.com>
2018-05-21 15:13:27 -0700

    - Updated MCS to work with recent Mimi changes - Removed file loading from EmpiricalDistribution - Added wip/load_empirical_dist.jl with convenience funcs for use with EmpiricalDistribution - Added generic "outer loop" processing for iteration over static values outside an MCS

---
+++ wip/load_empirical_dist.jl
@@ -0,0 +1,17 @@
+using FileIO
+using ExcelReaders
+using Mimi: EmpiricalDistribution
+
+function load_vector(path, range, header=false)
+    tups = collect(load(path, range, header=header))
+    name = fieldnames(tups[1])[1]   # field name of first item in NamedTuple
+    map(obj -> getfield(obj, name), tups)
+end
+
+function load_empirical_dist(excel_file::AbstractString, 
+                             values_range::AbstractString, 
+                             probs_range::AbstractString="")                               
+    values = load_vector(excel_file, values_range)
+    probs = probs_range == "" ? nothing : load_vector(excel_file, probs_range)
+    return EmpiricalDistribution(values, probs)
+end
