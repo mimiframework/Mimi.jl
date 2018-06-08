8ffb4ab425041556209dba9e75734ce263949a4c
lrennels <lrennels@berkeley.edu>
2018-06-07 22:24:43 -0700

    Improve final output formatting

--- benchmark/main.jl
+++ benchmark/main.jl
@@ -3,12 +3,12 @@ using PkgBenchmark
 function benchmarkMimi(target::String, baseline::String; filename::String = nothing)
 
     results = judge("Mimi", target, baseline) 
+    results_nums = collect(PkgBenchmark.benchmarkgroup(results))
 
-    base_results = PkgBenchmark.baseline_result(results)
-    base_trials = PkgBenchmark.benchmarkgroup(base_results)["region_models"]
-    target_results = PkgBenchmark.target_result(results)
-    target_trials = PkgBenchmark.benchmarkgroup(target_results)["region_models"]
-
+    println("Results Summary: ")
+    for i = 1:length(results_nums)
+        println(results_nums[i]...)
+    end
     println("Baseline Minimum:  ", base_trials)
     println("Target Minimum:  ", target_trials)
 
