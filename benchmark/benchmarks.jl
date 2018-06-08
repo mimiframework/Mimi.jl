d9d91bcaeb8aa944ffae8ccce5010b8bee900277
lrennels <lrennels@berkeley.edu>
2018-06-07 22:00:44 -0700

    Remove extra comments

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,6 +6,3 @@ using RegionTutorialBenchmarks
 const SUITE = BenchmarkGroup()
 SUITE["one_region"] = @benchmarkable run_oneregion() 
 SUITE["two_regions"] = @benchmarkable run_tworegion() 
-
-# SUITE["region_models"]["one-region"] = @benchmarkable run_oneregion()
-# SUITE["region_models"]["two-region"] = @benchmarkable run_tworegion()
