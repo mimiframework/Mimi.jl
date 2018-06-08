e558ea21cc750c619e83cca67179fa989f15e910
lrennels <lrennels@berkeley.edu>
2018-06-07 21:59:12 -0700

    Add two-region model back in

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,8 +5,7 @@ using RegionTutorialBenchmarks
 
 const SUITE = BenchmarkGroup()
 SUITE["one_region"] = @benchmarkable run_oneregion() 
+SUITE["two_regions"] = @benchmarkable run_tworegion() 
 
-# SUITE["region_models"] = BenchmarkGroup(["one-region", "two-region"])
 # SUITE["region_models"]["one-region"] = @benchmarkable run_oneregion()
 # SUITE["region_models"]["two-region"] = @benchmarkable run_tworegion()
-tune!(SUITE)
