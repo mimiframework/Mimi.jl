5c361f1142550ad7b47164affd0f992f25620e00
lrennels <lrennels@berkeley.edu>
2018-06-07 21:10:41 -0700

    Remove commits

--- benchmark/RegionTutorialBenchmarks.jl
+++ benchmark/RegionTutorialBenchmarks.jl
@@ -4,8 +4,6 @@ const tutorialpath = normpath(joinpath(@__DIR__, "..", "examples", "tutorial"))
 const tworegionpath = joinpath(tutorialpath, "02-two-region-model")
 const oneregionpath = joinpath(tutorialpath, "01-one-region-model")
 
-# 1 + 1
-
 include(joinpath(oneregionpath, "one-region-model.jl"))
 include(joinpath(tworegionpath, "main.jl"))
 
