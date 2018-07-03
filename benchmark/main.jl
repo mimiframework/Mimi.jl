d0317666049932fba36f1e72b4fb4976bfc83d3b
lrennels <lrennels@berkeley.edu>
2018-07-03 14:38:21 -0700

    Debugging

--- benchmark/main.jl
+++ benchmark/main.jl
@@ -4,7 +4,7 @@ using PkgBenchmark
 #at the target and the baeline git id (eg. branch, commit, etc.)
 function benchmarkMimi(target::String, baseline::String; filepath::String = "")
 
-    results = judge("Mimi", target, baseline)
+    results = judge("Mimi", target, baseline, retune = true)
 
     #gather the results in order to print a short summmary of the comparison
     trial_judgement = collect(PkgBenchmark.benchmarkgroup(results))
@@ -25,4 +25,19 @@ function benchmarkMimi(target::String, baseline::String; filepath::String = "")
     end
 
     return results
+end
+
+trials = []
+targets = []
+baselines = []
+for i = 1:5
+    results = judge("Mimi", b, b);
+    
+    trial_judgement = collect(PkgBenchmark.benchmarkgroup(results))
+    target_results_nums = collect(PkgBenchmark.benchmarkgroup(PkgBenchmark.target_result(results)))
+    baseline_results_nums = collect(PkgBenchmark.benchmarkgroup(PkgBenchmark.baseline_result(results)))
+
+    push!(trials, trial_judgement)
+    push!(targets, target_results_nums)
+    push!(baselines, baseline_results_nums)
 end
