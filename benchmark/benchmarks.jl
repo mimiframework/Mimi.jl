fe8f040270283e8b12c62fdb65a70a411e5c5d00
lrennels <lrennels@berkeley.edu>
2018-07-03 15:39:22 -0700

    Fix seconds kwarg for @benchmarkable

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -4,8 +4,8 @@ include("RegionTutorialBenchmarks.jl")
 using RegionTutorialBenchmarks
 
 const SUITE = BenchmarkGroup()
-SUITE["one_region"] = @benchmarkable run_oneregion() 
-SUITE["two_regions"] = @benchmarkable run_tworegion() 
+SUITE["one_region"] = @benchmarkable run_oneregion() seconds = 20.0 samples = 10_000
+SUITE["two_regions"] = @benchmarkable run_tworegion() seconds = 20.0 samples = 10_000
 
 # Run the following lines to tune the SUITE, view parameters, and save them to be 
 # viewed:   
--- benchmark/main.jl
+++ benchmark/main.jl
@@ -4,7 +4,8 @@ using PkgBenchmark
 #at the target and the baeline git id (eg. branch, commit, etc.)
 function benchmarkMimi(target::String, baseline::String; filepath::String = "")
 
-    results = judge("Mimi", target, baseline, retune = true)
+    #the default parameter for 
+    results = judge("Mimi", target, baseline)
 
     #gather the results in order to print a short summmary of the comparison
     trial_judgement = collect(PkgBenchmark.benchmarkgroup(results))
@@ -27,11 +28,13 @@ function benchmarkMimi(target::String, baseline::String; filepath::String = "")
     return results
 end
 
+# Temporary test
 trials = []
 targets = []
 baselines = []
-for i = 1:5
-    results = judge("Mimi", b, b, retune = true);
+b ="benchmark-branch1"
+for i = 1:10
+    results = judge("Mimi", b, b);
     trial_judgement = collect(PkgBenchmark.benchmarkgroup(results))
     target_results_nums = collect(PkgBenchmark.benchmarkgroup(PkgBenchmark.target_result(results)))
     baseline_results_nums = collect(PkgBenchmark.benchmarkgroup(PkgBenchmark.baseline_result(results)))
