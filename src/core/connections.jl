5da1fcaedd4779ab7c813c7e145efe8d28ce8cd5
lrennels <lrennels@berkeley.edu>
2018-07-16 14:34:08 -0700

    Change optional arg to comp_name

--- src/core/connections.jl
+++ src/core/connections.jl
@@ -199,26 +199,9 @@ function set_leftover_params!(md::ModelDef, parameters::Dict{T, Any}) where T
         # check whether we need to set the external parameter
         if ! haskey(md.external_params, param_name)
             value = parameters[string(param_name)]
-            param_dims = parameter_dimensions(md, comp_name, param_name)
-            num_dims = length(param_dims)
 
-            set_external_param!(md, param_name, value; param_dims = param_dims, num_dims = num_dims)
-            
-            #TODO:  if tests pass and we like this change to set_external_param!, delete the commented out code below
-            # if num_dims == 0    # scalar case
-            #     set_external_scalar_param!(md, param_name, value)
+            set_external_param!(md, param_name, value; comp_name = comp_name)
 
-            # else
-            #     if num_dims in (1, 2) && param_dims[1] == :time   # array case
-            #         value = convert(Array{md.number_type}, value)
-
-            #         values = get_timestep_instance(md, eltype(value), num_dims, value)
-                    
-            #     else
-            #         values = value
-            #     end
-            #     set_external_array_param!(md, param_name, values, param_dims)
-            # end
         end
         connect_parameter(md, comp_name, param_name, param_name)
     end
@@ -261,15 +244,18 @@ function add_external_param_conn(md::ModelDef, conn::ExternalParameterConnection
     push!(md.external_param_conns, conn)
 end
 
-function set_external_param!(md::ModelDef, name::Symbol, value::ModelParameter; param_dims = [], num_dims = 0)
+function set_external_param!(md::ModelDef, name::Symbol, value::ModelParameter)
     md.external_params[name] = value
 end
 
-function set_external_param!(md::ModelDef, name::Symbol, value::Number; param_dims = [], num_dims = 0)
+function set_external_param!(md::ModelDef, name::Symbol, value::Number; comp_name = comp_name)
     set_external_scalar_param!(md, name, value)
 end
 
-function set_external_param!(md::ModelDef, name::Symbol, value::Union{AbstractArray, Range, Tuple}; param_dims = [], num_dims = 0)
+function set_external_param!(md::ModelDef, name::Symbol, value::Union{AbstractArray, Range, Tuple}; comp_name = comp_name)
+    
+    param_dims = parameter_dimensions(md, comp_name, name)
+    num_dims = length(param_dims)
 
     if num_dims in (1, 2) && param_dims[1] == :time   
         value = convert(Array{md.number_type}, value)
--- src/core/model.jl
+++ src/core/model.jl
@@ -62,18 +62,18 @@ function connect_parameter(m::Model, dst::Pair{Symbol, Symbol}, src::Pair{Symbol
     connect_parameter(m.md, dst[1], dst[2], src[1], src[2], backup; ignoreunits=ignoreunits, offset=offset)
 end
 
-function set_external_param!(m::Model, name::Symbol, value::ModelParameter; param_dims = [], num_dims = 0)
+function set_external_param!(m::Model, name::Symbol, value::ModelParameter)
     set_external_param!(m.md, name, value)
     decache(m)
 end
 
-function set_external_param!(m::Model, name::Symbol, value::Number; param_dims = [], num_dims = 0)
-    set_external_param!(m.md, name, value)
+function set_external_param!(m::Model, name::Symbol, value::Number; comp_name = comp_name)
+    set_external_param!(m.md, name, value; comp_name = comp_name)
     decache(m)
 end
 
-function set_external_param!(m::Model, name::Symbol, value::Union{AbstractArray, Range, Tuple}; param_dims = [], num_dims = 0)
-    set_external_param!(m.md, name, value; param_dims = param_dims, num_dims = num_dims)
+function set_external_param!(m::Model, name::Symbol, value::Union{AbstractArray, Range, Tuple}; comp_name = comp_name)
+    set_external_param!(m.md, name, value; comp_name = comp_name)
 end
 
 function add_internal_param_conn(m::Model, conn::InternalParameterConnection)
