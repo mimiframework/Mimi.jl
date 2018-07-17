e9edf0a8eb6e293c9d9a25ec7225eeb0c125a600
lrennels <lrennels@berkeley.edu>
2018-07-17 16:50:58 -0700

    Add small changes to main.jl

--- benchmark/main.jl
+++ benchmark/main.jl
@@ -2,7 +2,9 @@ using PkgBenchmark
 
 #function to compare the package benchmark suite performance at the target git id
 #at the target and the baeline git id (eg. branch, commit, etc.)
-function benchmarkMimi(target::String, baseline::String; filepath::String = "")
+filepath = "/Users/lisarennels/.julia/v0.6/Mimi/benchmark/judge_results.md"
+
+function benchmarkMimi(target::String, baseline::String; filepath::String = filepath)
 
     #the default parameter for 
     results = judge("Mimi", target, baseline)
@@ -20,26 +22,24 @@ function benchmarkMimi(target::String, baseline::String; filepath::String = "")
         println("")
     end
 
-    #print optional results file
-    if filepath != ""
-        export_markdown(string(filepath), results)
-    end
+    #print results file
+    export_markdown(string(filepath), results)
 
     return results
 end
 
-# Temporary test
-trials = []
-targets = []
-baselines = []
-b ="benchmark-branch1"
-for i = 1:10
-    results = judge("Mimi", b, b);
-    trial_judgement = collect(PkgBenchmark.benchmarkgroup(results))
-    target_results_nums = collect(PkgBenchmark.benchmarkgroup(PkgBenchmark.target_result(results)))
-    baseline_results_nums = collect(PkgBenchmark.benchmarkgroup(PkgBenchmark.baseline_result(results)))
+# # Temporary test
+# trials = []
+# targets = []
+# baselines = []
+# b ="benchmark-branch1"
+# for i = 1:10
+#     results = judge("Mimi", b, b);
+#     trial_judgement = collect(PkgBenchmark.benchmarkgroup(results))
+#     target_results_nums = collect(PkgBenchmark.benchmarkgroup(PkgBenchmark.target_result(results)))
+#     baseline_results_nums = collect(PkgBenchmark.benchmarkgroup(PkgBenchmark.baseline_result(results)))
 
-    push!(trials, trial_judgement)
-    push!(targets, target_results_nums)
-    push!(baselines, baseline_results_nums)
-end
+#     push!(trials, trial_judgement)
+#     push!(targets, target_results_nums)
+#     push!(baselines, baseline_results_nums)
+# end
