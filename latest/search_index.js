eff3632082c1c4d05236f6ef1cdac5bd3d562dd7
autodocs <autodocs>
2018-07-24 02:40:39 +0000

    build based on 42c517a

--- latest/search_index.js
+++ latest/search_index.js
@@ -225,6 +225,14 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "userguide.html#The-init-function-1",
+    "page": "User Guide",
+    "title": "The init function",
+    "category": "section",
+    "text": "The init function can optionally be called within @defcomp and before run_timestep.  Similarly to run_timestep, this function is called with parameters init(p, v, d), where the component state (defined by the first three arguments) has fields for the Parameters, Variables, and Dimensions of the component you defined.   If defined for a specific component, this function will run before the timestep loop, and should only be used for parameters or variables without a time index e.g. to compute the values of scalar variables that only depend on scalar parameters. Note that when using init, it may be necessary to add special handling in the run_timestep function for the first timestep, in particular for difference equations.  A skeleton @defcomp script using both run_timestep and init would appear as follows:@defcomp component1 begin\n\n    # First define the state this component will hold\n    savingsrate = Parameter()\n\n    # Second, define the (optional) init function for the component\n    function init(p, v, d)\n    end\n\n    # Third, define the run_timestep function for the component\n    function run_timestep(p, v, d, t)\n    end\n\nend"
+},
+
+{
     "location": "userguide.html#The-explorer-UI-1",
     "page": "User Guide",
     "title": "The explorer UI",
