f7855acb6de868adeb2685e67971472c3c1dbaaa
lrennels <lrennels@berkeley.edu>
2018-08-26 15:46:44 -0700

    Complete dataframe testing

--- test/test_getdataframe.jl
+++ test/test_getdataframe.jl
@@ -4,16 +4,20 @@ using Mimi
 using Base.Test
 
 import Mimi:
-    reset_compdefs
+    reset_compdefs, _load_dataframe
 
 reset_compdefs()
 
+#
+# Test with > 2 dimensions
+#
+
 my_model = Model()
 
-#Testing that you cannot add two components of the same name
 @defcomp testcomp1 begin
     var1 = Variable(index=[time])
     par1 = Parameter(index=[time])
+    par_scalar = Parameter()
     
     function run_timestep(p, v, d, t)
         v.var1[t] = p.par1[t]
@@ -32,7 +36,6 @@ end
 @defcomp testcomp3 begin
     var3 = Variable(index=[time])
     par3 = Parameter(index=[time])
-    
     function run_timestep(p, v, d, t)
         v.var3[t] = p.par3[t]
     end
@@ -43,6 +46,7 @@ par = collect(2015:5:2110)
 set_dimension!(my_model, :time, 2015:5:2110)
 add_comp!(my_model, testcomp1)
 set_param!(my_model, :testcomp1, :par1, par)
+set_param!(my_model, :testcomp1, :par_scalar, 5.)
 
 # Test running before model built
 @test_throws ErrorException dataframe = getdataframe(my_model, :testcomp1, :var1)
@@ -56,6 +60,12 @@ dataframe = getdataframe(my_model, :testcomp1, :var1)
 
 # Test trying to getdataframe from component that does not exist
 @test_throws ErrorException getdataframe(my_model, :testcomp1, :var2)
+
+# Test trying to load an item into an existing dataframe where that item key already exists
+@test_throws ErrorException _load_dataframe(my_model, :testcomp1, :var1, dataframe)
+
+# Test trying to load a dataframe for a scalar
+@test_throws ErrorException _load_dataframe(my_model, :testcomp1, :par_scalar, dataframe)
 
 #
 # Test with > 2 dimensions
@@ -86,7 +96,10 @@ set_param!(new_model, :testcomp4, :par1, data)
 
 run(new_model)
 
-# Test 3 getting DF for a dimensional parameter
-@test size(getdataframe(new_model, :testcomp4, :par1)) == (8, 4)
+df = getdataframe(new_model, :testcomp4, :par1)
+@test size(df) == (8, 4)
+
+# Test trying to combine two items with different dimensions into one dataframe
+@test_throws ErrorException getdataframe(new_model, Pair(:testcomp4, :par1), Pair(:testcomp4, :var3))
 
 end #module
