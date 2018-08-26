ded42dcd94826ec20f2de0b917295a16311d1c06
lrennels <lrennels@berkeley.edu>
2018-08-25 22:30:50 -0700

    Remove test_plotting depenence on Mimi ConnectorCompVector

--- test/test_plotting.jl
+++ test/test_plotting.jl
@@ -1,4 +1,11 @@
+module TestPlotting
+
 using Mimi
+
+import Mimi: 
+    reset_compdefs
+
+reset_compdefs()
 
 @defcomp LongComponent begin
     x = Parameter(index=[time])
@@ -19,20 +26,32 @@ end
     end
 end
 
+@defcomp ConnectorComponent begin
+    input1 = Parameter(index = [time])
+    input2 = Parameter(index = [time])
+    output =  Variable(index = [time])
+
+    function run_timestep(p, v, d, ts)
+        v.output[ts] = p.input2[ts]
+    end
+end
+
 m = Model()
 set_dimension!(m, :time, 2000:3000)
 nsteps = Mimi.dim_count(m.md, :time)
 
 add_comp!(m, ShortComponent; first=2100)
-add_comp!(m, ConnectorCompVector, :MyConnector) # can give it your own name
+add_comp!(m, ConnectorComponent)
 add_comp!(m, LongComponent; first=2000)
 
 set_param!(m, :ShortComponent, :a, 2.)
 set_param!(m, :LongComponent, :y, 1.)
-connect_param!(m, :MyConnector, :input1, :ShortComponent, :b)
-set_param!(m, :MyConnector, :input2, zeros(nsteps))
-connect_param!(m, :LongComponent, :x, :MyConnector, :output)
+connect_param!(m, :ConnectorComponent, :input1, :ShortComponent, :b)
+set_param!(m, :ConnectorComponent, :input2, zeros(nsteps))
+connect_param!(m, :LongComponent, :x, :ConnectorComponent, :output)
 
 run(m)
 
 plot_comp_graph(m)
+
+end #module
