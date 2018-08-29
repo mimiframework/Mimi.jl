c259d047ef6bf221cdf80917ffc72747cbf22f21
Cora Kingdon <ckingdon@berkeley.edu>
2018-08-29 14:31:46 -0400

    Add tests

--- test/test_parametertypes.jl
+++ test/test_parametertypes.jl
@@ -4,7 +4,7 @@ using Mimi
 using Base.Test
 
 import Mimi: 
-    external_params, update_external_param, TimestepMatrix, TimestepVector, 
+    external_params, update_external_param!, TimestepMatrix, TimestepVector, 
     ArrayModelParameter, ScalarModelParameter, FixedTimestep, reset_compdefs
 
 reset_compdefs()
@@ -84,21 +84,88 @@ extpars = external_params(m)
 @test typeof(extpars[:h].value) == numtype
 
 # test updating parameters
-@test_throws ErrorException update_external_param(m, :a, 5) # expects an array
-@test_throws ErrorException update_external_param(m, :a, ones(101)) # wrong size
-@test_throws ErrorException update_external_param(m, :a, fill("hi", 101, 3)) # wrong type
-update_external_param(m, :a, Array{Int,2}(zeros(101, 3))) # should be able to convert from Int to Float
 
-@test_throws ErrorException update_external_param(m, :d, ones(5)) # wrong type; should be scalar
-update_external_param(m, :d, 5) # should work, will convert to float
+@test_throws ErrorException update_external_param!(m, :a, 5) # expects an array
+@test_throws ErrorException update_external_param!(m, :a, ones(101)) # wrong size
+@test_throws ErrorException update_external_param!(m, :a, fill("hi", 101, 3)) # wrong type
+update_external_param!(m, :a, Array{Int,2}(zeros(101, 3))) # should be able to convert from Int to Float
+
+@test_throws ErrorException update_external_param!(m, :d, ones(5)) # wrong type; should be scalar
+update_external_param!(m, :d, 5) # should work, will convert to float
 @test extpars[:d].value == 5
-@test_throws ErrorException update_external_param(m, :e, 5) # wrong type; should be array
-@test_throws ErrorException update_external_param(m, :e, ones(10)) # wrong size
-update_external_param(m, :e, [4,5,6,7])
+@test_throws ErrorException update_external_param!(m, :e, 5) # wrong type; should be array
+@test_throws ErrorException update_external_param!(m, :e, ones(10)) # wrong size
+update_external_param!(m, :e, [4,5,6,7])
 
 @test length(extpars) == 8
 @test typeof(extpars[:a].values) == TimestepMatrix{FixedTimestep{2000, 1}, numtype}
 @test typeof(extpars[:d].value) == numtype
 @test typeof(extpars[:e].values) == Array{numtype, 1}
+
+
+#------------------------------------------------------------------------------
+# Test updating TimestepArrays with update_external_parameter
+#------------------------------------------------------------------------------
+
+using Mimi
+using Base.Test
+import Mimi: update_external_param!
+@defcomp MyComp2 begin 
+    x=Parameter(index=[time]) 
+    y=Variable(index=[time])
+    function run_timestep(p,v,d,t)
+        v.y[t]=p.x[t]
+    end
+end
+
+# 1. Test with Fixed Timesteps
+
+m = Model()
+set_dimension!(m, :time, 2000:2002)
+add_comp!(m, MyComp2)
+set_param!(m, :MyComp2, :x, [1, 2, 3])
+set_dimension!(m, :time, 2001:2003)
+
+update_external_param!(m, :x, [4, 5, 6], update_timesteps = false)
+x = m.md.external_params[:x]
+@test x.values isa Mimi.TimestepArray{Mimi.FixedTimestep{2000, 1, LAST} where LAST, Float64, 1}
+@test x.values.data == [4., 5., 6.]
+run(m)
+@test m[:MyComp2, :y][1] == 5   # 2001
+@test m[:MyComp2, :y][2] == 6   # 2002
+
+update_external_param!(m, :x, [2, 3, 4], update_timesteps = true)
+x = m.md.external_params[:x]
+@test x.values isa Mimi.TimestepArray{Mimi.FixedTimestep{2001, 1, LAST} where LAST, Float64, 1}
+@test x.values.data == [2., 3., 4.]
+run(m)
+@test m[:MyComp2, :y][1] == 2   # 2001
+@test m[:MyComp2, :y][2] == 3   # 2002
+
+
+# 2. Test with Variable Timesteps
+
+m = Model()
+set_dimension!(m, :time, [2000, 2005, 2020])
+add_comp!(m, MyComp2)
+set_param!(m, :MyComp2, :x, [1, 2, 3])
+set_dimension!(m, :time, [2005, 2020, 2050])
+
+update_external_param!(m, :x, [4, 5, 6], update_timesteps = false)
+x = m.md.external_params[:x]
+@test x.values isa Mimi.TimestepArray{Mimi.VariableTimestep{(2000, 2005, 2020)}, Float64, 1}
+@test x.values.data == [4., 5., 6.]
+run(m)
+@test m[:MyComp2, :y][1] == 5   # 2005
+@test m[:MyComp2, :y][2] == 6   # 2020
+
+update_external_param!(m, :x, [2, 3, 4], update_timesteps = true)
+x = m.md.external_params[:x]
+@test x.values isa Mimi.TimestepArray{Mimi.VariableTimestep{(2005, 2020, 2050)}, Float64, 1}
+@test x.values.data == [2., 3., 4.]
+run(m)
+@test m[:MyComp2, :y][1] == 2   # 2005
+@test m[:MyComp2, :y][2] == 3   # 2020
+
 
 end #module
