427ec04f35c7c11cc892475dbe61d717d2ca5858
David Anthoff <anthoff@berkeley.edu>
2018-10-12 11:36:12 -0700

    Add benchmarking code

---
+++ benchmark/RegionTutorialBenchmarks.jl
@@ -0,0 +1,11 @@
+const tutorialpath = normpath(joinpath(@__DIR__, "..", "examples", "tutorial"))
+const tworegionpath = joinpath(tutorialpath, "02-two-region-model")
+const oneregionpath = joinpath(tutorialpath, "01-one-region-model")
+
+function run_oneregion()
+    include(joinpath(oneregionpath, "one-region-model.jl"))
+end
+
+function run_tworegion()
+    include(joinpath(tworegionpath, "main.jl"))
+end
---
+++ benchmark/benchmarks.jl
@@ -0,0 +1,13 @@
+using BenchmarkTools
+
+include("RegionTutorialBenchmarks.jl")
+
+const SUITE = BenchmarkGroup()
+SUITE["one_region"] = @benchmarkable run_oneregion() # seconds = 20.0 samples = 10_000
+SUITE["two_regions"] = @benchmarkable run_tworegion() # seconds = 20.0 samples = 10_000
+
+# Run the following lines to tune the SUITE, view parameters, and save them to be 
+# viewed:   
+# tune!(SUITE)
+# SUITE
+# BenchmarkTools.save("params.json", params(suite));
