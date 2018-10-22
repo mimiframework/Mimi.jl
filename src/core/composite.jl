fda38cbbd34de30c415301043310e531ac44fe7c
Richard Plevin <rich@plevin.com>
2018-10-21 23:14:15 -0700

    WIP - composite components

--- src/core/composite.jl
+++ src/core/composite.jl
@@ -52,17 +52,6 @@ struct CompositeComponentDef <: AbstractComponentDef
     exports::Vector{Pair{Symbol, Tuple{ComponentId, Symbol}}}
 end
 
-
-# This function is called by the default run_timestep defined by @defcomp when 
-# the user defines sub-components and doesn't define an explicit run_timestep.
-# Can it also be called by the user's run_timestep?
-function _composite_run_timestep(p, v, d, t)
-    for ci in components(mci)
-        run_timestep(ci, t)
-    end
-    return nothing
-end
-
 abstract struct AbstractComponentInstance end
 
 mutable struct LeafComponentInstance{TV <: ComponentInstanceVariables, TP <: ComponentInstanceParameters} <: AbstractComponentInstance
@@ -92,13 +81,11 @@ mutable struct LeafComponentInstance{TV <: ComponentInstanceVariables, TP <: Com
 
         comp_module = eval(Main, comp_id.module_name)
 
-        # the try/catch allows components with no run_timestep function (as in some of our test cases)
-        if is_composite
-            self.run_timestep = _composite_run_timestep
-        else
-            self.run_timestep = try eval(comp_module, Symbol("run_timestep_$(comp_id.module_name)_$(comp_id.comp_name)")) end
-        end
-           
+        # The try/catch allows components with no run_timestep function (as in some of our test cases)
+        # All CompositeComponentInstances use a standard method that just loops over inner components.
+        mod_and_comp = "$(comp_id.module_name)_$(comp_id.comp_name)"
+        self.run_timestep = is_composite ? nothing else try eval(comp_module, Symbol("run_timestep_$(mod_and_comp)")) end           
+        self.init         = is_composite ? nothing else try eval(comp_module, Symbol("init_$(mod_and_comp)")) end
         return self
     end
 end
@@ -137,8 +124,7 @@ struct Model
     end
 end
 
-# If using composition with LeafComponentInstance, we just delegate from 
-# CompositeComponentInstance to the internal (summary) LeafComponentInstance.
+# We delegate from CompositeComponentInstance to the internal LeafComponentInstance.
 compid(ci::LeafComponentInstance) = ci.comp_id
 name(ci::LeafComponentInstance) = ci.comp_name
 dims(ci::LeafComponentInstance) = ci.dim_dict
@@ -152,8 +138,6 @@ timestep_func(ci::LeafComponentInstance) = ci.run_timestep
 @delegate dims(ci::CompositeComponentInstance) => leaf
 @delegate variables(ci::CompositeComponentInstance) => leaf
 @delegate parameters(ci::CompositeComponentInstance) => leaf
-@delegate init_func(ci::LeafComponentInstance) => leaf
-@delegate timestep_func(ci::CompositeComponentInstance) => leaf
 
 @delegate variables(m::Model) => cci
 @delegate parameters(m::Model) => cci
@@ -162,47 +146,123 @@ timestep_func(ci::LeafComponentInstance) = ci.run_timestep
 @delegate lasts(m::Model) => cci
 @delegate clocks(m::Model) => cci
 
-function reset(cci::CompositeComponentInstance)
-    for c in components(mci)
-        reset(c)
+function reset_variables(cci::CompositeComponentInstance)
+    for ci in components(cci)
+        reset_variables(ci)
     end
     return nothing
 end
 
-function run_timestep(ci::C, t::T) where {C <: AbstractComponentInstance, T <: AbstractTimestep}
+function init(ci::LeafComponentInstance)
+    reset_variables(ci)
+
+    fn = init_func(ci)
+    if fn != nothing
+        fn(parameters(ci), variables(ci), dims(ci))
+    end
+    return nothing
+end
+
+function init(cci::CompositeComponentInstance)
+    for ci in components(cci)
+        init(ci)
+    end
+    return nothing
+end
+
+function run_timestep(ci::LeafComponentInstance, clock::Clock)
     fn = timestep_func(ci)
     if fn != nothing
-        fn(parameters(ci), variables(ci), dims(ci), t)
+        fn(parameters(ci), variables(ci), dims(ci), clock.ts)
+    end
+
+    # TBD: move this outside this func if components share a clock
+    advance(clock)
+
+    return nothing
+end
+
+function run_timestep(cci::CompositeComponentInstance, clock::Clock)
+    for ci in components(cci)
+        run_timestep(ci, clock)
     end
     return nothing
 end
 
+ # splitarg produces a tuple for each arg of the form (arg_name, arg_type, slurp, default)
+_arg_name(arg_tup) = arg_tup[1]
+_arg_type(arg_tup) = arg_tup[2]
+_arg_slurp(arg_tup) = arg_tup[3]
+_arg_default(arg_tup) = arg_tup[4]
+
+function _extract_args(args, kwargs)
+    valid_kws = (:exports, :bindings)    # valid keyword args to `component()`
+    kw_values = Dict()
+
+    arg_tups = map(splitarg, args)
+
+    if kwargs === nothing
+        # If a ";" was not used to separate kwargs, extract them from args.
+        # tup[4] => "default" value which for kwargs, the actual value.
+        kwarg_tups = filter!(tup -> _arg_default(tup) !== nothing, arg_tups)
+    else
+        kwarg_tups = map(splitarg, kwargs)
+    end
+
+    @info "args: $arg_tups"
+    @info "kwargs: $kwarg_tups"
+
+    if 1 > length(arg_tups) > 2
+        @error "component() must have one or two non-keyword values"
+    end
+
+    arg1 = _arg_name(arg_tups[1])
+    arg2 = length(args) == 2 ? _arg_name(arg_tups[2]) : nothing
+
+    for tup in kwarg_tups
+        arg_name = _arg_name(tup)
+        if arg_name in valid_kws
+            default = _arg_default(tup)
+            if hasmethod(Base.iterate, (typeof(default),))
+                append!(kw_values[arg_name], default)
+            else
+                @error "Value of $arg_name argument must be iterable"
+            end
+
+        else
+            @error "Unknown keyword $arg_name; valid keywords are $valid_kws"
+        end
+    end
+
+    @info "kw_values: $kw_values"
+    return (arg1, arg2, kw_values)
+end
 
 """
     defcomposite(cc_name::Symbol, ex::Expr)
 
-Define a Mimi CompositeComponent `ccc_name` with the expressions in `ex`.  Expressions
+Define a Mimi CompositeComponent `cc_name` with the expressions in `ex`.  Expressions
 are all variations on `component(...)`, which adds a component to the composite. The
-calling signature for `component` is:
+calling signature for `component()` processed herein is:
 
     `component(comp_id::ComponentId, name::Symbol=comp_id.comp_name; 
-               export::Union{Void,Vector}, bind::Union{Void,Vector{Pair}})`
+               exports::Union{Void,Vector}, bindings::Union{Void,Vector{Pair}})`
 
 In this macro, the vector of symbols to export is expressed without the `:`, e.g.,
-`export=[var_1, var_2, param_1])`. The names must be variable or parameter names in
+`exports=[var_1, var_2, param_1])`. The names must be variable or parameter names in
 the component being added.
 
 Bindings are expressed as a vector of `Pair` objects, where the first element of the
-pair is a symbol (without the `:` prefix) representing a parameter in the component
+pair is the name (again, without the `:` prefix) representing a parameter in the component
 being added, and the second element is either a numeric constant, a matrix of the
 appropriate shape, or the name of a variable in another component. The variable name
 is expressed as the component id (which may be prefixed by a module, e.g., `Mimi.adder`)
 followed by a `.` and the variable name in that component. So the form is either
 `modname.compname.varname` or `compname.varname`, which must be known in the current module.
 
-Unlike LeafComponents, CompositeComponents do not have user-defined `init` and `run_timestep`
-functions; these are defined internally to simply iterate over constituent components and
-call the associated method on each.
+Unlike LeafComponents, CompositeComponents do not have user-defined `init` or `run_timestep`
+functions; these are defined internally to iterate over constituent components and call
+the associated method on each.
 """
 macro defcomposite(cc_name, ex)
     @capture(ex, elements__)
@@ -220,45 +280,63 @@ macro defcomposite(cc_name, ex)
         push!(let_block, expr)
     end
 
+    valid_kws = (:exports, :bindings)    # valid keyword args to `component()`
+    kw_values = Dict()
+
     for elt in elements
         offset = 0
 
-        if @capture(elt, component(comp_mod_name_name_.comp_name_)    | component(comp_name_) |
-                         component(comp_mod_name_.comp_name_, alias_) | component(comp_name_, alias_))
+        if @capture(elt, (component(args__; kwargs__) | component(args__)))
+
+            #   component(comp_mod_name_.comp_name_) | 
+            #   component(comp_mod_name_.comp_name_, alias_)))
+
+            # splitarg produces a tuple for each arg of the form (arg_name, arg_type, slurp, default)
+            arg_tups = map(splitarg, args)
+
+            if kwargs === nothing
+                # If a ";" was not used to separate kwargs, extract them from args.
+                # tup[4] => "default" value which for kwargs, the actual value.
+                kwarg_tups = filter!(tup -> tup[4] !== nothing, arg_tups)
+            else
+                kwarg_tups = map(splitarg, kwargs)
+            end
+
+            @info "args: $args"
+            @info "kwargs: $kwargs"
+
+            if 1 > length(args) > 2
+                @error "component() must have one or two non-keyword values"
+            end
+
+            arg1 = args[1]
+            arg2 = length(args) == 2 ? args[2] else nothing
+
+            for (arg_name, arg_type, slurp, default) in kwarg_tups
+                if arg_name in valid_kws
+                    if hasmethod(Base.iterate, (typeof(default),)
+                        append!(kw_values[arg_name], default)
+                    else
+                        @error "Value of $arg_name argument must be iterable"
+                    end
+
+                else
+                    @error "Unknown keyword $arg_name; valid keywords are $valid_kws"
+                end
+            end
+
+            @info "kw_values: $kw_values"
 
             # set local copy of comp_mod_name to the stated or default component module
             expr = (comp_mod_name === nothing ? :(comp_mod_name = nameof(calling_module)) : :(comp_mod_name = $(QuoteNode(comp_mod_name))))
             addexpr(expr)
 
-            name = (alias === nothing ? comp_name : alias)
-            expr = :(add_comp!($cc_name, eval(comp_mod_name).$comp_name, $(QuoteNode(name))))
-
-        # TBD: extend comp.var syntax to allow module name, e.g., FUND.economy.ygross
-        elseif (@capture(elt, src_comp_.src_name_[arg_] => dst_comp_.dst_name_) ||
-                @capture(elt, src_comp_.src_name_ => dst_comp_.dst_name_))
-            if (arg !== nothing && (! @capture(arg, t - offset_) || offset <= 0))
-                error("Subscripted connection source must have subscript [t - x] where x is an integer > 0")
-            end
-
-            expr = :(Mimi.connect_param!($cc_name,
-                                         $(QuoteNode(dst_comp)), $(QuoteNode(dst_name)),
-                                         $(QuoteNode(src_comp)), $(QuoteNode(src_name)), 
-                                         offset=$offset))
-
-        elseif @capture(elt, index[idx_name_] = rhs_)
-            expr = :(Mimi.set_dimension!($cc_name, $(QuoteNode(idx_name)), $rhs))
-
-        elseif @capture(elt, comp_name_.param_name_ = rhs_)
-            expr = :(Mimi.set_param!($cc_name, $(QuoteNode(comp_name)), $(QuoteNode(param_name)), $rhs))
-
-        else
-            # Pass through anything else to allow the user to define intermediate vars, etc.
-            println("Passing through: $elt")
-            expr = elt
+            # name = (alias === nothing ? comp_name : alias)
+            # expr = :(add_comp!($cc_name, eval(comp_mod_name).$comp_name, $(QuoteNode(name))))
+            
+            expr = :(CompositeComponentDef($comp_id, $comp_name, $comps, bindings=$bindings, exports=$exports))
+            addexpr(expr)
         end
-        
-        expr = :(CompositeComponentDef($comp_id, $comp_name, $comps, bindings=$bindings, exports=$exports))
-        addexpr(expr)
     end
 
 
