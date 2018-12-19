707617982313f62d23d4ba44711caf871093dd1a
lrennels <lrennels@berkeley.edu>
2018-12-19 14:49:28 -0800

    Fix dependency paths; FUND path is temporary

--- test/test_dependencies.jl
+++ test/test_dependencies.jl
@@ -10,8 +10,8 @@ end
 
 #list of URLs of branches of packages to test
 dependencies = [
-    "https://github.com/lrennels/fund/archive/1768edf12aaaac3a41bbea081d5b51299121f993.zip",
-    "https://github.com/lrennels/mimi-rice-2010.jl/archive/2b5996b0a0c8be92290991f045c43af425c5a9c8.zip"
+    "https://github.com/ckingdon95/fund/archive/9bf61c315b52685128bf1ae40179e245e12d8213",
+    "https://github.com/anthofflab/mimi-rice-2010.jl/archive/2b5996b0a0c8be92290991f045c43af425c5a9c8.zip"
 ]
 
 function run_dependency_tests(dependencies=dependencies)
