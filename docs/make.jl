cde942eaf5926496a892ff79cadbc2b736be3511
lrennels <lrennels@berkeley.edu>
2018-12-21 13:32:39 -0800

    Correct backslashes in make.jl
    Fix page titles
    
    Fix ref
    
    Fix backslash

--- docs/make.jl
+++ docs/make.jl
@@ -8,11 +8,11 @@ makedocs(
 		"Installation Guide" => "installation.md",
 		"User Guide" => "userguide.md",
 		"Tutorials" => Any[
-			"Introduction" => "tutorials\tutorial_main.md",
-			"1. Run an Existing Model " => "tutorials\tutorial_main.md",
-			"2. Modfiy an Existing Model" => "tutorials\tutorial_main.md",
-			"3. Create a Model" => "tutorials\tutorial_main.md",
-			"4. MCS Functionality" => "tutorials\tutorial_main.md"
+			"Tutorials Intro" => "tutorials/tutorial_main.md",
+			"1 Run an Existing Model" => "tutorials/tutorial_main.md",
+			"2 Modfiy an Existing Model" => "tutorials/tutorial_main.md",
+			"3 Create a Model" => "tutorials/tutorial_main.md",
+			"4 MCS Functionality" => "tutorials/tutorial_main.md"
 		],
 		"FAQ" => "faq.md",
 		"Reference" => "reference.md",
