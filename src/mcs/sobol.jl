7cb3c5777932d8d59e45a9c75fcb97ad498e92b8
lrennels <lrennels@berkeley.edu>
2019-01-23 16:48:25 -0800

    Start adding Sobol functionality

--- src/mcs/sobol.jl
+++ src/mcs/sobol.jl
@@ -1,7 +1,7 @@
-# Rough draft for Lisa to complete
 struct SobolData <: AbstractSimulationData
     N::Int
     calc_second_order::Bool
+    samples::AbstractArray{<:Number, N} where N
 
     function SobolData(; N::Int=1000, calc_second_order::Bool=false)
         new(N, calc_second_order)
@@ -16,8 +16,21 @@ end
 const SobolSimulation = Simulation{SobolData}
 
 function sample!(sim::SobolSimulation)
-    # sim.trials = N * whatever...
+    sim.trials = size(sim.samples,1)
+    rvdict = sim.rvdict
+    num_rvs = length(rvdict)
+    rvlist = sim.dist_rvs
+
+    for (i, rv) in enumerate(rvlist)
+        dist = rv.dist
+        name = rv.name
+        values = sim.samples[:, i]
+        rvdict[name] = RandomVariable(name, SampleStore(values))
+    end
 end
 
 function analyze(sim::SobolSimulation)
+    # analysis done with the SALib.jl repo - we could eventually run that from
+    # within Mimi but for now we will just take the outputs of the runs and 
+    # manually work with the SALib.jl repo functions
 end
