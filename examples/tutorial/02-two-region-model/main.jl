78957a08c5528978cab2272224f754e1efed8c6d
Lisa Rennels <31779240+lrennels@users.noreply.github.com>
2019-02-15 17:42:21 -0800

    Correct relative  file path

--- examples/tutorial/02-two-region-model/main.jl
+++ examples/tutorial/02-two-region-model/main.jl
@@ -1,6 +1,6 @@
 using Mimi
 
-include("/Users/lisarennels/.julia/dev/Mimi/examples/tutorial/02-two-region-model/two-region-model.jl")
+include("two-region-model.jl")
 using .MyModel
 model = construct_MyModel()
 
