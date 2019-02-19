eed54f359e7015b2553cb8151b6331decdc84c2a
lrennels <lrennels@berkeley.edu>
2019-02-18 17:28:39 -0800

    Remove save_vega_plot fcn; Add docs note on .vegalite format

--- src/explorer/explore.jl
+++ src/explorer/explore.jl
@@ -67,12 +67,3 @@ function explore(m::Model, comp_name::Symbol, datum_name::Symbol)
 
     return VegaLite.VLSpec{:plot}(spec)
 end
-
-"""
-    function vega_plot(filepath::String, p::VegaLite.VLSpec{:plot})
-
-Save a the plot `p` as `filepath`.  Simple wrapper function for VegaLite's `save` function.
-"""
-function save_vega_plot(filepath::String, p::VegaLite.VLSpec{:plot})
-    VegaLite.save(filepath, p);
-end
