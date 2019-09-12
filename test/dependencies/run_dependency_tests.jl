83722c861a74195d5c89fb3b897d6d961e021335
David Anthoff <anthoff@berkeley.edu>
2019-09-12 13:54:52 -0700

    Change how we do dependency testing

--- test/dependencies/run_dependency_tests.jl
+++ test/dependencies/run_dependency_tests.jl
@@ -1,6 +1,18 @@
 using Pkg
 
-Pkg.instantiate()
+packages_to_test = [
+    ("https://github.com/anthofflab/MimiRICE2010.jl.git", "v2.0.3", "MimiRICE2010"),
+    ("https://github.com/fund-model/MimiFUND.jl.git", "v3.11.5", "MimiFUND")
+]
 
-Pkg.test("MimiRICE2010")
-Pkg.test("MimiFUND")
+for (pkg_url, pkg_rev, pkg_name) in packages_to_test
+    mktempdir() do folder
+        Pkg.activate(folder)
+
+        Pkg.develop(joinpath(@__DIR__, "..", ".."))
+
+        Pkg.add(PackageSpec(url=pkg_url, rev=pkg_rev))
+
+        Pkg.test(pkg_name)
+    end
+end
