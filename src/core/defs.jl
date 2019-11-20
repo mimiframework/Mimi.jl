2c378461b095bb61d455f8e1d0b7c8d97afacfae
Lisa Rennels <31779240+lrennels@users.noreply.github.com>
2019-11-20 10:34:31 -0800

    Undo change to variable_dimensions doctstring.

--- src/core/defs.jl
+++ src/core/defs.jl
@@ -578,10 +578,7 @@ unit(obj::ParameterDefReference) = parameter(obj).unit
     variable_dimensions(obj::AbstractCompositeComponentDef, comp_path::ComponentPath, var_name::Symbol)
 
 Return the names of the dimensions of variable `var_name` exposed in the composite 
-component definition indicated by`obj` along the component path `comp_path`. The 
-`comp_path`is of type `Mimi.ComponentPath` with the single argument being a named
-tuple of symbols describing the relative (to a composite) or absolute (relative to ModelDef) 
-path through composite nodes to specific composite or leaf node.
+component definition indicated by`obj` along the component path `comp_path`.
 """
 function variable_dimensions(obj::AbstractCompositeComponentDef, comp_path::ComponentPath, var_name::Symbol)
     var = variable(obj, comp_path, var_name)
