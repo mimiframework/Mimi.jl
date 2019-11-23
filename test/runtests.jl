4b3ef974a275ddacf5f303f8ddb6d1e681805ec8
David Anthoff <anthoff@berkeley.edu>
2019-11-22 17:45:53 -0800

    Prepare tests for Github Actions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,8 @@
 using Mimi
+import Electron
 using Test
+
+Electron.prep_test_env()
 
 @testset "Mimi" begin
 
@@ -30,7 +33,7 @@ using Test
     @info("test_model_structure.jl")
     include("test_model_structure.jl")
 
-    @info("test_model_structure_variabletimestep.jl") 
+    @info("test_model_structure_variabletimestep.jl")
     include("test_model_structure_variabletimestep.jl")
 
     @info("test_replace_comp.jl")
@@ -54,7 +57,7 @@ using Test
     @info("test_getindex.jl")
     include("test_getindex.jl")
 
-    @info("test_getindex_variabletimestep.jl") 
+    @info("test_getindex_variabletimestep.jl")
     include("test_getindex_variabletimestep.jl")
 
     @info("test_components.jl")
@@ -66,14 +69,14 @@ using Test
     @info("test_getdataframe.jl")
     include("test_getdataframe.jl")
 
-    @info("test_mult_getdataframe.jl")        
-    include("test_mult_getdataframe.jl")    
+    @info("test_mult_getdataframe.jl")
+    include("test_mult_getdataframe.jl")
 
     @info("test_clock.jl")
     include("test_clock.jl")
- 
-    @info("test_timesteps.jl")           
-    include("test_timesteps.jl") 
+
+    @info("test_timesteps.jl")
+    include("test_timesteps.jl")
 
     @info("test_timesteparrays.jl")
     include("test_timesteparrays.jl")
@@ -83,7 +86,7 @@ using Test
 
     @info("test_datum_storage.jl")
     include("test_datum_storage.jl")
-    
+
     @info("test_connectorcomp.jl")
     include("test_connectorcomp.jl")
 
