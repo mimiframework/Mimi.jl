45f98dac8d6c91a0d2b6279f599f3d45bddc744c
lrennels <lrennels@berkeley.edu>
2020-03-03 14:00:03 -0800

    Update RICE dependency path to mimi-next fork

--- test/dependencies/run_dependency_tests.jl
+++ test/dependencies/run_dependency_tests.jl
@@ -1,7 +1,7 @@
 using Pkg
 
 packages_to_test = [
-    ("https://github.com/anthofflab/MimiRICE2010.jl.git", "v2.0.3", "MimiRICE2010"),
+    ("https://github.com/lrennels/mimi-rice-2010.jl.git", "mimi-next", "MimiRICE2010"),
     ("https://github.com/fund-model/MimiFUND.jl.git", "mimi-next", "MimiFUND")
 ]
 
