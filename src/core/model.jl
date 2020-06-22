b957ee7cc07912dfbf552258ab7d373a73c7633c
Cora Kingdon <ckingdon@berkeley.edu>
2020-06-22 15:46:58 -0400

    fix docstring

--- src/core/model.jl
+++ src/core/model.jl
@@ -77,7 +77,7 @@ function connect_param!(m::Model, dst::Pair{Symbol, Symbol}, src::Pair{Symbol, S
 end
 
 """
-    disconnect_param!(m:Model, comp_name::Symbol, param_name::Symbol)
+    disconnect_param!(m::Model, comp_name::Symbol, param_name::Symbol)
 
 Remove any parameter connections for a given parameter `param_name` in a given component
 `comp_def` in model `m`.
