32300fcfb211738c9d9a7ed56383b7636d1ef2ba
lrennels <lrennels@berkeley.edu>
2020-09-24 18:34:43 -0700

    Add hooks to the delta method

--- src/mcs/mcs.jl
+++ src/mcs/mcs.jl
@@ -7,11 +7,13 @@ using StatsBase
 using IterTools
 
 include("mcs_types.jl")
+include("mcs_utils.jl")
 include("EmpiricalDistribution.jl")
 include("montecarlo.jl")
 include("lhs.jl")
 include("sobol.jl")
 include("defmcs.jl")
+include("delta.jl")
 
 export 
     @defsim, generate_trials!, run, save_trial_inputs, _save_trial_results, set_models!,
--- src/mcs/sobol.jl
+++ src/mcs/sobol.jl
@@ -29,8 +29,15 @@ function _compute_trials(sim_inst::SobolSimulationInstance, N::Int)
     sim_inst.trials = N * (factor * num_rvs + 2)
 end
 
-# Use original distribution when resampling from SampleStore
-_get_dist(rv::RandomVariable) = (rv.dist isa SampleStore ? rv.dist.dist : rv.dist)
+function create_GSA_payload(sim_inst::SobolSimulationInstance)
+    rv_info = OrderedDict{Symbol, Any}([name => _get_dist(rv) for (name, rv) in sim_inst.sim_def.rvdict])
+
+    # back out N
+    N = _compute_N(sim_inst)
+
+    # create payload
+    return GlobalSensitivityAnalysis.SobolData(params = rv_info, calc_second_order = sim_inst.sim_def.data.calc_second_order, N = N)
+end
 
 function sample!(sim_inst::SobolSimulationInstance, samplesize::Int)
     rvdict = sim_inst.sim_def.rvdict
@@ -59,14 +66,4 @@ function analyze(sim_inst::SobolSimulationInstance, model_output::AbstractArray{
     payload = create_GSA_payload(sim_inst)
 
     return GlobalSensitivityAnalysis.analyze(payload, model_output; num_resamples = num_resamples, conf_level = conf_level, N_override = N_override, progress_meter = progress_meter)
-end
-
-function create_GSA_payload(sim_inst::SobolSimulationInstance)
-    rv_info = OrderedDict{Symbol, Any}([name => _get_dist(rv) for (name, rv) in sim_inst.sim_def.rvdict])
-
-    # back out N
-    N = _compute_N(sim_inst)
-
-    # create payload
-    return GlobalSensitivityAnalysis.SobolData(params = rv_info, calc_second_order = sim_inst.sim_def.data.calc_second_order, N = N)
 end
